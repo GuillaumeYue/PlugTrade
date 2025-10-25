bd8f0a858fcc54df72ec5319fe35c9215847756c
shaquille neil <shaquille-36@hotmail.com>
2025-10-24 23:52:36 -0400

    Initial Commit

---
+++ PlugTrade/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlugTrade
+//
+//  Created by Shaquille O Neil on 2025-10-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlugTrade/PlugTradeApp.swift
@@ -0,0 +1,29 @@
+//
+//  PlugTradeApp.swift
+//  PlugTrade
+//
+//  Created by Shaquille O Neil on 2025-10-24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct PlugTradeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
