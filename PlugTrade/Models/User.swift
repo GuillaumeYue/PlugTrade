4c8c8d12ac450877aad96af20809376e6b95bfa5
shaquille neil <shaquille-36@hotmail.com>
2025-10-28 19:06:47 -0400

    added the profile and sign up

---
+++ PlugTrade/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  PlugTrade
+//
+//  Created by Shaquille O Neil on 2025-10-27.
+//
+
+import Foundation
---
+++ PlugTrade/Views/ImageuploadButton.swift
@@ -0,0 +1,18 @@
+//
+//  ImageuploadButton.swift
+//  PlugTrade
+//
+//  Created by Shaquille O Neil on 2025-10-28.
+//
+
+import SwiftUI
+
+struct ImageuploadButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageuploadButton()
+}
